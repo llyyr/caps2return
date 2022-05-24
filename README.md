24056d8bd7e93d81d4194635f7d02c68039c2d60
llyyr <llyyr@users.noreply.github.com>
2022-05-25 01:06:46 +0530

    Make ctrl+capslock behave as capslock

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # caps2return
 
-caps2return is an [Interception Tools](https://gitlab.com/interception/linux/tools) plugin that changes KEY_CAPSLOCK events to KEY_ENTER events. Works on Wayland/X11/TTY.
+caps2return is an [Interception Tools](https://gitlab.com/interception/linux/tools) plugin that changes KEY_CAPSLOCK events to KEY_ENTER events and maps LCTRL+CAPSLOCK to CAPSLOCK. Works on Wayland/X11/TTY.
 
 ## Install
 
@@ -19,6 +19,6 @@ $ cmake --build build
 - JOB: intercept -g $DEVNODE | caps2return | uinput -d $DEVNODE
   DEVICE:
     EVENTS:
-      EV_KEY: [KEY_CAPSLOCK]
+      EV_KEY: [KEY_CAPSLOCK, KEY_LEFTCTRL]
 ```
 
--- caps2return.c
+++ caps2return.c
@@ -1,34 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <unistd.h>
 #include <linux/input.h>
 
-int read_event(struct input_event *event) {
-	return fread(event, sizeof(struct input_event), 1, stdin) == 1;
+int read_event(struct input_event *event)
+{
+    return fread(event, sizeof(struct input_event), 1, stdin) == 1;
 }
 
-void write_event(const struct input_event *event) {
-	if (fwrite(event, sizeof(struct input_event), 1, stdout) != 1) {
-		exit(EXIT_FAILURE);
-	}
+void write_event(const struct input_event *event)
+{
+    if (fwrite(event, sizeof(struct input_event), 1, stdout) != 1)
+        exit(EXIT_FAILURE);
 }
 
-int main() {
-	struct input_event input;
-	setbuf(stdin, NULL), setbuf(stdout, NULL);
+int main()
+{
+    struct input_event input;
+    const struct input_event
+        syn = {.type = EV_SYN, .code = SYN_REPORT, .value = 0},
+        capslock_up = {.type = EV_KEY, .code = KEY_CAPSLOCK, .value = 0},
+        capslock_down = {.type = EV_KEY, .code = KEY_CAPSLOCK, .value = 1};
+    enum {
+        START, 
+        CTRL_HELD 
+    } state = START;
+    setbuf(stdin, NULL), setbuf(stdout, NULL);
 
-	while (read_event(&input)) {
-		if (input.type == EV_MSC && input.code == MSC_SCAN) {
-			continue;
-		}
-
-		if (input.type == EV_KEY && input.code == KEY_CAPSLOCK) {
-			struct input_event enter = {.type = EV_KEY, .code = KEY_ENTER, .value = input.value};
-			write_event(&enter);
-			continue;
-		}
-
-		write_event(&input);
-	}
+    while (read_event(&input)) {
+        if (input.type == EV_MSC && input.code == MSC_SCAN)
+            continue;
+        switch (state) {
+            case START:
+                if (input.type == EV_KEY && input.code == KEY_LEFTCTRL && input.value == 1)
+                    state = CTRL_HELD;
+                if (input.type == EV_KEY && input.code == KEY_CAPSLOCK)
+                    input.code = KEY_ENTER;
+                break;
+            case CTRL_HELD:
+                if (input.type == EV_KEY && input.code == KEY_CAPSLOCK && input.value == 1) {
+                    write_event(&capslock_down);
+                    write_event(&syn);
+                    write_event(&capslock_up);
+                    continue;
+                }
+                if (input.type == EV_KEY && input.code == KEY_CAPSLOCK && input.value == 0)
+                    continue;
+                if (input.type == EV_KEY && input.code == KEY_LEFTCTRL && input.value == 0)
+                    state = START;
+                break;
+        }
+        write_event(&input);
+    }
 }
